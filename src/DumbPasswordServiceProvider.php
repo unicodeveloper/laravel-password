16d2764a01ceccdf639015d7e33f817389603608
Joey Kudish <joey@jkudish.com>
2017-04-25 16:16:15 -0700

    add caching
    cache the password list for future re-use so it doesn’t need to be re-computed every time

--- src/DumbPasswordServiceProvider.php
+++ src/DumbPasswordServiceProvider.php
@@ -35,12 +35,13 @@ class DumbPasswordServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $path = realpath(__DIR__.'/../resources/config/passwordlist.txt');
-        $dumbPasswords = collect(explode("\n", file_get_contents($path)));
-        $data = $dumbPasswords->flip();
-
-        Validator::extend('dumbpwd', function ($attribute, $value, $parameters, $validator) use ($data) {
-            return !$data->has($value);
+         Validator::extend('dumbpwd', function ($attribute, $value, $parameters, $validator) {
+            $path = realpath(__DIR__ . '/../resources/config/passwordlist.txt');
+            $cache_key = md5_file($path);
+            $data = Cache::rememberForever('dumbpwd_list_' . $cache_key, function () use ($path) {
+                return collect(explode("\n", file_get_contents($path)));
+            });
+            return !$data->contains($value);
         }, $this->message);
     }
 
