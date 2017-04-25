e75dad64eafd135954d097bb16713441727c1d57
Joey Kudish <joachim.kudish@gmail.com>
2017-04-25 15:42:22 -0700

    flip the array of pwds before collecting it
    If flipped after it's been made into a collection, then 2 collections exist in memory, and with 10k items, that consumes a lot of memory. Instead, this performs the flip before it puts into a collection

--- src/DumbPasswordServiceProvider.php
+++ src/DumbPasswordServiceProvider.php
@@ -29,8 +29,7 @@ class DumbPasswordServiceProvider extends ServiceProvider
     public function boot()
     {
         $path = realpath(__DIR__.'/../resources/config/passwordlist.txt');
-        $dumbPasswords = collect(explode("\n", file_get_contents($path)));
-        $data = $dumbPasswords->flip();
+        $data = collect(array_flip(explode("\n", file_get_contents($path))));
 
         Validator::extend('dumbpwd', function ($attribute, $value, $parameters, $validator) use ($data) {
             return !$data->has($value);
